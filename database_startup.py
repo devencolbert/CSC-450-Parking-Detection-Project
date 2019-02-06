52eff2681115f778a25c1e59eb954153bd841d77
John Guinn <jguinn124@gmail.com>
2019-02-06 13:18:29 -0600

    File added to start database engine

---
+++ database_startup.py
@@ -0,0 +1,9 @@
+import os
+import sqlalchemy_declarative
+import sqlalchemy_insert
+
+python sqlalchemy_declarative
+python sqlalchemy_insert
+
+#execfile('sqlalchemy_declarative.py')
+#execfile('sqlalchemy_insert.py')
