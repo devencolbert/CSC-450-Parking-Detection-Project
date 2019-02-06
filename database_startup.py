9c5d1dbd51a9a8034d018c9d6ddf879f5679de39
John Guinn <jguinn124@gmail.com>
2019-02-06 13:25:50 -0600

    Update database_startup.py

--- database_startup.py
+++ database_startup.py
@@ -2,8 +2,6 @@ import os
 import sqlalchemy_declarative
 import sqlalchemy_insert
 
-python sqlalchemy_declarative
-python sqlalchemy_insert
 
-#execfile('sqlalchemy_declarative.py')
-#execfile('sqlalchemy_insert.py')
+execfile('sqlalchemy_declarative.py')
+execfile('sqlalchemy_insert.py')
