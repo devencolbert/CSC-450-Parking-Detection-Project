f0f96e93a52cbfd23f59800b2f5c45a3377e951a
devencolbert <31320471+devencolbert@users.noreply.github.com>
2019-02-06 22:21:18 -0600

    Changes to file.py
    Can get a small bounding box on the corner of the screen.

--- file.py
+++ file.py
@@ -1,18 +1,30 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('test.jpg')
+img = cv2.imread('test3.jpg')
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-img = cv2.GaussianBlur(imgray, (5,5), 0)
-img = cv2.Canny(img, 50, 150)
-ret,thresh = cv2.threshold(img,127,255,0)
+blur = cv2.bilateralFilter(imgray,9,75,75)
+edges = cv2.Canny(blur, 50, 150, apertureSize = 3)
+'''
+minLineLength = 100
+maxLineGap = 100
+lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+for x1,y1,x2,y2 in lines[0]:
+    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+
+'''
+ret,thresh = cv2.threshold(edges,127,255,0)
 contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-cnt = contours[0]
-x,y,w,h = cv2.boundingRect(cnt)
-img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+cnt = contours[4]
 
-img = cv2.drawContours(img, contours, -1, (150,255,0), 3)
+
+rect = cv2.minAreaRect(cnt)
+box = cv2.boxPoints(rect)
+box = np.int0(box)
+img = cv2.drawContours(img,[box],0,(0,255,0),2)
+
+#img = cv2.drawContours(img, [cnt], 0, (150,255,0), 3)
 
 cv2.imshow("window",img)
 cv2.waitKey(0)
