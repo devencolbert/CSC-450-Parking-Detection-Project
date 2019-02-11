d7a38eb95b8e45ee2aa237a3388276d1f2c4447a
elzieconnor <31282129+elzieconnor@users.noreply.github.com>
2019-02-11 09:59:25 -0600

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
-def index_page():
-    return 'Real-Time Parking Space Detection'
+def index():
+    return render_template('index.html')
