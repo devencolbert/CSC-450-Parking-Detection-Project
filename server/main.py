a6d9dc5a26e28fef4be3030715feeef094bc26b5
elzieconnor <31282129+elzieconnor@users.noreply.github.com>
2019-02-13 15:19:05 -0600

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import requests
 
 app = Flask(__name__)
+
+app.config['DATABASE URI'] = 'sqlite:///sqlalchemy_example.db'
+db =    SQLAlchemy(app)
+
+class User(db.Model):
+        id - db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(80), unique=True, nullable=False)
+        email = db.Column(db.String(120), unique=True, nullable=False)
+        
+        def __repr__(self):
+               return '<User %r>' % self.username
 
 @app.route('/')
 def index():
