2ca6adae96b5284741bc77828db0e760f70e7a1f
devencolbert <31320471+devencolbert@users.noreply.github.com>
2019-02-17 13:26:38 -0600

    Changes to camera_client.py
    Added in encoding and decoding of a frame.

--- camera_client.py
+++ camera_client.py
@@ -2,6 +2,9 @@ import numpy as np
 import cv2
 import math
 import pickle
+import matplotlib.pyplot as plt
+import importlib
+import os, glob
 
 cap = cv2.VideoCapture(0)
 
@@ -11,8 +14,8 @@ out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
 frameRate = cap.get(5) #frame rate
 x=1
-decimg = ""
-
+i=0
+decimg = []
 while(cap.isOpened()):
     frameId = cap.get(10) 
     ret, frame = cap.read()
@@ -22,16 +25,20 @@ while(cap.isOpened()):
 
         filename = './test' +  str(int(x)) + ".png";x+=1
         cv2.imwrite(filename, frame)
-        encode_param = [int(cv2.IMWRITE_PNG_COMPRESSION), 0]
+        encode_param = [int(cv2.IMWRITE_PNG_COMPRESSION), 9]
         results, encimg = cv2.imencode('.png', frame, encode_param)
-
+        #images = [plt.imread(path) for path in glob.glob('test/*.jpg')]
         decimg = cv2.imdecode(encimg, 1)
+        #i += 1
 
-        cv2.imshow('frame',decimg)
+        cv2.imshow('frame',frame)
+        cv2.imshow('Decoded image',decimg)
         if cv2.waitKey(0) & 0xFF == ord('q'):
             break
     else:
         break
+
+
 # Release everything if job is finished
 cap.release()
 out.release()
