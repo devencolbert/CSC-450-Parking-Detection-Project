55e8fb71bbbe686bb2968c641cf800f5a1a7af50
John Guinn <jguinn124@gmail.com>
2019-02-24 13:40:27 -0600

    Testing

---
+++ main/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+#from flask_sqlalchemy import SQLAlchemy
+#import requests
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlalchemy_example.db'
+#db = SQLAlchemy(app)
+'''
+class Parking(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        position = db.Column(db.String(10), unique=True, nullable=False)
+        availability = db.Column(db.Integer(10), unique=True, nullable=False)
+        
+        def __repr__(self):
+               return '<Parking %r>' % self.username
+'''
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/view')
+def view():
+    return render_template('view.html')
---
+++ main/static:styles/style.css
@@ -0,0 +1,11 @@
+body            { font-family: calibri; background: #eee; }
+a, h1, h2       { color: #d300dd; }
+h1, h2          { font-family: 'Georgia', serif; margin: 0; color: #d300dd; }
+h1              { border-bottom: 2px solid #eee; }
+h2              { font-size: 1.2em; }
+
+#banner         { margin-top: 80px; position: relative; width: 100%; height: 300px; background: #000; }
+#banner img     { max-height: 300px; }
+.title          { text-align: center; }
+
+#content        { margin-top: 380px; position: relative; width: auto; height: auto; background: #eee; }
