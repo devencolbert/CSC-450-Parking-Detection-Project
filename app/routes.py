0fe4f7ec55f491436592f075b3412d28ca164202
elzieconnor <31282129+elzieconnor@users.noreply.github.com>
2019-03-02 15:05:33 -0600

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import Flask, request, render_template
+import sqlite3
+
+DATABASE = 'C:\\Users\\Connor\\Documents\\project\\app.db'
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Lot")
+    data = cur.fetchall()
+    return render_template('index.html', data=data)
