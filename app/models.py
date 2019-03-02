e5c2267c2b672a4c238db558412cb3d799296f33
elzieconnor <31282129+elzieconnor@users.noreply.github.com>
2019-03-02 10:33:46 -0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
 from app import db
 
-class Lot(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(64), index=True)
-    spots = db.relationship('Spot', backref='author', lazy='dynamic')
+class Lot(db.Model): #Create LOTS table
+    id = db.Column(db.Integer, primary_key=True) #Create ID column as PRIMARY KEY
+    title = db.Column(db.String(64), index=True) #Create title column
+    spots = db.relationship('Spot', backref='author', lazy='dynamic') #Create spots column
 
     def __repr__(self):
         return '<Lot {}>'.format(self.username)  
 
-class Spot(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    availability = db.Column(db.String(140))
-    lot_id = db.Column(db.Integer, db.ForeignKey('lot.id'))
+class Spot(db.Model): #Create SPOTS table
+    id = db.Column(db.Integer, primary_key=True) #Create ID column as PRIMARY KEY
+    availability = db.Column(db.Boolean(140)) #Create availability column as boolean
+    lot_id = db.Column(db.Integer, db.ForeignKey('lot.id')) #Create lot_id column as FOREIGN KEY
 
     def __repr__(self):
         return '<Spot {}>'.format(self.body)
