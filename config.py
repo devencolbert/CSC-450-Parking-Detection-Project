5bc9a41bc8b83680f41979b2d00e6aefb171d329
elzieconnor <31282129+elzieconnor@users.noreply.github.com>
2019-03-03 17:28:40 -0600

    Added secret key

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 db_uri = 'sqlite:///{}'.format(basedir)
 
 class Config(object):
-    SECRET_KEY = 'you-will-never-guess'
+    SECRET_KEY = 'in-case-we-decide-to-use-this'
     SQLALCHEMY_DATABASE_URI = db_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
