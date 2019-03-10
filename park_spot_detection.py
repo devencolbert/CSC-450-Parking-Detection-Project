e488647121da569011c577d4593a19f32bb948f1
devencolbert <31320471+devencolbert@users.noreply.github.com>
2019-03-10 15:29:52 -0500

    Added code to parking_spot_detection.py
    Transfered some of the practice code to the image processing software file.

--- park_spot_detection.py
+++ park_spot_detection.py
@@ -1,97 +1,239 @@
+import yaml
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import importlib
 import os, glob
-import camera_client
+#import camera_client
+import file4
 
 #moduleName = input('Enter module name: ')
 #importlib.import_module(moduleName)
+fn = "output.mp4"
+fn_yaml = "parking_spots.yml"
+cascade_src = 'cars.xml'
+car_cascade = cv2.CascadeClassifier(cascade_src)
+global_str = "Last change at: "
+change_pos = 0.00
 
-def all_images(images, cmap=None):
-    cols = 3
-    rows = (len(images)+1)//cols
+#video info for processing the footage
+cap = cv2.VideoCapture(fn)
+video_info = {  'fps':    cap.get(cv2.CAP_PROP_FPS),
+                'width':  int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)*0.6),
+                'height': int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)*0.6),
+                'fourcc': cap.get(cv2.CAP_PROP_FOURCC),
+                'num_of_frames': int(cap.get(cv2.CAP_PROP_FRAME_COUNT))}
 
-    plt.figure(figsize=(15, 12))
+cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-    for i, image in enumerate(images):
-        plt.subplot(rows, cols, i+1)
-        #cmap = 'gray' if len(image.shape)==2 else cmap
-        plt.imshow(image, cmap='gray')
-        plt.xticks([])
-        plt.yticks([])
-    plt.tight_layout(pad=0, h_pad=0, w_pad=0)
-    plt.show()
+def run_classifier(img, id):
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cars = car_cascade.detectMultiScale(img, 1.1, 1)
+    if cars == ():
+        return False
+    else:
+        # parking_status[id] = False
+        return True
+fgbg = cv2.createBackgroundSubtractorMOG2(history=300, varThreshold=16, detectShadows=True)
 
-images = [plt.imread(path) for path in glob.glob('test/*.jpg')]
-#all_images(images)
+with open(fn_yaml, 'r') as stream:
+    parking_data = yaml.load(stream)
+parking_contours = []
+parking_bounding_rects = []
+parking_mask = []
+parking_data_motion = []
+if parking_data != None:
+    for park in parking_data:
+        points = np.array(park['points'])
+        rect = cv2.boundingRect(points)
+        points_shifted = points.copy()
+        points_shifted[:,0] = points[:,0] - rect[0] # shift contour to region of interest
+        points_shifted[:,1] = points[:,1] - rect[1]
+        parking_contours.append(points)
+        parking_bounding_rects.append(rect)
+        mask = cv2.drawContours(np.zeros((rect[3], rect[2]), dtype=np.uint8), [points_shifted], contourIdx=-1,
+                                    color=255, thickness=-1, lineType=cv2.LINE_8)
+        mask = mask==255
+        parking_mask.append(mask)
 
-def mask_images(image):
-    #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_white = np.array([0,200,0])
-    upper_white = np.array([255,255,255])
-    white = cv2.inRange(image, lower_white, upper_white)
+kernel_erode = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3)) # morphological kernel
+kernel_dilate = cv2.getStructuringElement(cv2.MORPH_RECT,(5,19))
+if parking_data != None:
+    parking_status = [False]*len(parking_data)
+    parking_buffer = [None]*len(parking_data)
+
+def print_parkIDs(park, coor_points, frame_rev):
+    moments = cv2.moments(coor_points)
+    centroid = (int(moments['m10']/moments['m00'])-3, int(moments['m01']/moments['m00'])+3)
+    # putting numbers on marked regions
+    cv2.putText(frame_rev, str(park['id']), (centroid[0]+1, centroid[1]+1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+    cv2.putText(frame_rev, str(park['id']), (centroid[0]-1, centroid[1]-1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+    cv2.putText(frame_rev, str(park['id']), (centroid[0]+1, centroid[1]-1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+    cv2.putText(frame_rev, str(park['id']), (centroid[0]-1, centroid[1]+1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+    cv2.putText(frame_rev, str(park['id']), centroid, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
     
-    lower_yellow = np.array([10,0,100])
-    upper_yellow = np.array([255,255,255])
-    yellow = cv2.inRange(image, lower_yellow, upper_yellow)
-    
-    mask = cv2.bitwise_or(white, yellow)
-    mask = cv2.bitwise_and(image, image, mask=mask)
-    return mask
+while(cap.isOpened()):
+    video_cur_pos = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0 # Current position of the video file in seconds
+    video_cur_frame = cap.get(cv2.CAP_PROP_POS_FRAMES) # Index of the frame to be decoded/captured next
+    ret, frame_initial = cap.read()
+    if ret == True:
+        frame = cv2.resize(frame_initial, None, fx=0.6, fy=0.6)
+    if ret == False:
+        print("Video ended")
+        break
 
-mask_image = list(map(mask_images, images))
-#all_images(mask_image)
+# Background Subtraction
+    frame_blur = cv2.GaussianBlur(frame.copy(), (5,5), 3)
+    # frame_blur = frame_blur[150:1000, 100:1800]
+    frame_gray = cv2.cvtColor(frame_blur, cv2.COLOR_BGR2GRAY)
+    frame_out = frame.copy()
 
-def grayScale(image):
-    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # Drawing the Overlay. Text overlay at the left corner of screen
+    str_on_frame = "%d/%d" % (video_cur_frame, video_info['num_of_frames'])
+    cv2.putText(frame_out, str_on_frame, (5,30), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.8, (0,255,255), 2, cv2.LINE_AA)
+    cv2.putText(frame_out,global_str + str(round(change_pos,2)) + 'sec', (5, 60), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.8, (255, 0, 0), 2, cv2.LINE_AA)
 
-gray_images = list(map(grayScale, mask_image))
-#all_images(gray_images)
+# frame_blur = frame_blur[380:420, 240:470]
+# cv2.imshow('dss', frame_blur)
+    fgmask = fgbg.apply(frame_blur)
+    bw = np.uint8(fgmask==255)*255
+    bw = cv2.erode(bw, kernel_erode, iterations=1)
+    bw = cv2.dilate(bw, kernel_dilate, iterations=1)
+    # cv2.imshow('dss',bw)
+    # cv2.imwrite("frame%d.jpg" % co, bw)
+    (cnts, _) = cv2.findContours(bw.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # loop over the contours
+    for c in cnts:
+        # print(cv2.contourArea(c))
+        # if the contour is too small, we ignore it
+        if cv2.contourArea(c) < 500:
+            continue
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame_out, (x, y), (x + w, y + h), (255, 0, 0), 1)
 
-def blur_image(image):
-    return cv2.GaussianBlur(image, (5,5), 0)
+# detecting cars and vacant spaces
+    for ind, park in enumerate(parking_data):
+        points = np.array(park['points'])
+        rect = parking_bounding_rects[ind]
+        roi_gray = frame_gray[rect[1]:(rect[1]+rect[3]), rect[0]:(rect[0]+rect[2])] # crop roi for faster calcluation
 
-blur = list(map(blur_image, gray_images))
-#all_images(blur)
+        laplacian = cv2.Laplacian(roi_gray, cv2.CV_64F)
+        # cv2.imshow('oir', laplacian)
+        points[:,0] = points[:,0] - rect[0] # shift contour to roi
+        points[:,1] = points[:,1] - rect[1]
+        delta = np.mean(np.abs(laplacian * parking_mask[ind]))
+        # if(delta<2.5):
+            # print("ind, del", ind, delta)
+        status = delta < 2.2
+        # If detected a change in parking status, save the current time
+        if status != parking_status[ind] and parking_buffer[ind]==None:
+            parking_buffer[ind] = video_cur_pos
+            change_pos = video_cur_pos
+            # print("state ", ind,delta)
+            # applying classifier in case a change is detected in the status of area
+            # if dict['classifier_used']:
+            #     classifier_result = run_classifier(roi_gray)
+            #     if classifier_result:
+            #         print(classifier_result)
+        # If status is still different than the one saved and counter is open
+        elif status != parking_status[ind] and parking_buffer[ind]!=None:
+            if video_cur_pos - parking_buffer[ind] > 1:
+                parking_status[ind] = status
+                parking_buffer[ind] = None
+        # If status is still same and counter is open
+        elif status == parking_status[ind] and parking_buffer[ind]!=None:
+            parking_buffer[ind] = None
 
-def canny_detection(image, low =50, high=150):
-    return cv2.Canny(image, low, high)
+# changing the color on the basis on status change occured in the above section and putting numbers on areas
+    for ind, park in enumerate(parking_data):
+        points = np.array(park['points'])
+        if parking_status[ind]:
+            color = (0,255,0)
+            rect = parking_bounding_rects[ind]
+            roi_gray_ov = frame_gray[rect[1]:(rect[1] + rect[3]),
+                           rect[0]:(rect[0] + rect[2])]  # crop roi for faster calcluation
+            res = run_classifier(roi_gray_ov, ind)
+            if res:
+                parking_data_motion.append(parking_data[ind])
+                # del parking_data[ind]
+                color = (0,0,255)
+        else:
+            color = (0,0,255)
+        
+        cv2.drawContours(frame_out, [points], contourIdx=-1,
+                             color=color, thickness=2, lineType=cv2.LINE_8)
+        print_parkIDs(park, points, frame_out)
+            
+            
 
-edge = list(map(lambda image: canny_detection(image), blur))
-#all_images(edge)
+    if parking_data_motion != []:
+        for index, park_coord in enumerate(parking_data_motion):
+            points = np.array(park_coord['points'])
+            color = (0, 0, 255)
+            recta = parking_bounding_rects[ind]
+            roi_gray1 = frame_gray[recta[1]:(recta[1] + recta[3]),
+                            recta[0]:(recta[0] + recta[2])]  # crop roi for faster calcluation
+            # laplacian = cv2.Laplacian(roi_gray, cv2.CV_64F)
+            # delta2 = np.mean(np.abs(laplacian * parking_mask[ind]))
+            # state = delta2<1
+            # classifier_result = run_classifier(roi_gray1, index)
+            # cv2.imshow('dsd', roi_gray1)
+            fgbg1 = cv2.createBackgroundSubtractorMOG2(history=300, varThreshold=16, detectShadows=True)
+            roi_gray1_blur = cv2.GaussianBlur(roi_gray1.copy(), (5, 5), 3)
+            # cv2.imshow('sd', roi_gray1_blur)
+            fgmask1 = fgbg1.apply(roi_gray1_blur)
+            bw1 = np.uint8(fgmask1 == 255) * 255
+            bw1 = cv2.erode(bw1, kernel_erode, iterations=1)
+            bw1 = cv2.dilate(bw1, kernel_dilate, iterations=1)
+            # cv2.imshow('sd', bw1)
+            # cv2.imwrite("frame%d.jpg" % co, bw)
+            (_, cnts1, _) = cv2.findContours(bw1.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # loop over the contours
+            for c in cnts1:
+                print(cv2.contourArea(c))
+                # if the contour is too small, we ignore it
+                if cv2.contourArea(c) < 4:
+                    continue
+                (x, y, w, h) = cv2.boundingRect(c)
+                classifier_result1 = run_classifier(roi_gray1, index)
+                if classifier_result1:
+                # print(classifier_result)
+                    color = (0, 0, 255)  # Red again if car found by classifier
+                else:
+                    color = (0,255, 0)
+            classifier_result1 = run_classifier(roi_gray1, index)
+            if classifier_result1:
+                # print(classifier_result)
+                color = (0, 0, 255)  # Red again if car found by classifier
+            else:
+                color = (0, 255, 0)
+            cv2.drawContours(frame_out, [points], contourIdx=-1,
+                                 color=color, thickness=2, lineType=cv2.LINE_8)
 
-def lines(image):
-    return cv2.HoughLinesP(image, 1, np.pi/180, 100, np.array([]), minLineLength=100, maxLineGap=100)
+        # detect people in the image. Slows down the program, requires high GPU speed
+        (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        # draw the  bounding boxes
+        for (x, y, w, h) in rects:
+            cv2.rectangle(frame_out, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-parking_lines = list(map(lines, edge))
 
-def draw(image,linez):
-    draw_lines = np.copy(image)
+    # Display video
+    cv2.imshow('frame', frame_out)
+    # cv2.imshow('background mask', bw)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+    elif k == ord('c'):
+        cv2.imwrite('frame%d.jpg' % video_cur_frame, frame_out)
+    elif k == ord('j'):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, video_cur_frame+1000) # jump 1000 frames
+    elif k == ord('u'):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, video_cur_frame + 500)  # jump 500 frames
+    if cv2.waitKey(33) == 27:
+        break
 
-    for line in linez:
-        for x1, y1, x2, y2 in line:
-            cv2.line(draw_lines, (x1,y1),(x2,y2),(255,0,0),5)
-    return draw_lines
-
-line_images = []
-for image, linez in zip(images, parking_lines):
-    line_images.append(draw(image,linez))
-    
-#all_images(line_images)
-'''
-def contour(image):
-    c_lines = np.copy(image)
-    ret, thresh = cv2.threshold(c_lines,127,255,0)
-    contours, hierarchy = cv2.findContours(thresh, 1, 2)
-
-    cnt = conturs[0]
-    M = cv2.moments(cnt)
-    print(M)
-
-    #epsilon = 0.1*cv2.arcLength(cnt,True)
-    #approx = cv2.approxPolyDP(cnt,epsilon,True)
-    #return cv2.drawContours(contour_lines, contour, -1, (0,255,0),3)
-
-#rect = list(map(contour, line_images))
-'''
+cv2.waitKey(0)
+cap.release()
+cv2.destroyAllWindows()
