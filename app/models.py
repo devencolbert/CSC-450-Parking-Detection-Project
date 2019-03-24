9f8ca1a7f65d0869d1f4871768efbc358a372e55
elzieconnor <31282129+elzieconnor@users.noreply.github.com>
2019-03-24 12:43:19 -0500

    Add ModelView and updated database tables

--- app/models.py
+++ app/models.py
@@ -1,12 +1,17 @@
-from app import db
+from app import db, basic_auth
+from flask_admin.contrib import sqla
+from werkzeug.exceptions import HTTPException
 
 class Lot(db.Model): #Create LOTS table
     id = db.Column(db.Integer, primary_key=True) #Create ID column as PRIMARY KEY
     title = db.Column(db.String(64), index=True) #Create title column
+    percentage = db.Column(db.Integer, index=True) #Create percentage column
+    total_spaces = db.Column(db.Integer, index=True) #Create total spaces column
+    available_spaces = db.Column(db.Integer, index=True) #Create available spaces column
     spots = db.relationship('Spot', backref='author', lazy='dynamic') #Create spots column
 
     def __repr__(self):
-        return '<Lot {}>'.format(self.username)  
+        return '<Lot {}>'.format(self.title)  
 
 class Spot(db.Model): #Create SPOTS table
     id = db.Column(db.Integer, primary_key=True) #Create ID column as PRIMARY KEY
@@ -14,4 +19,22 @@ class Spot(db.Model): #Create SPOTS table
     lot_id = db.Column(db.Integer, db.ForeignKey('lot.id')) #Create lot_id column as FOREIGN KEY
 
     def __repr__(self):
-        return '<Spot {}>'.format(self.body)
+        return '<Spot {}>'.format(self.availability)
+
+class ModelView(sqla.ModelView):
+    def is_accessible(self):
+        if not basic_auth.authenticate():
+            raise AuthException('Not authenticated.')
+        else:
+            return True
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(basic_auth.challenge())
+
+class AuthException(HTTPException):
+    def __init__(self, message):
+        super().__init__(message, Response(
+            "You could not be authenticated. Please refresh the page.", 401,
+            {'WWW-Authenticate': 'Basic realm="Login Required"'}
+        ))
+
