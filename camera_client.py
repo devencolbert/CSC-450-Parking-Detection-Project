aed8b12c037f5988051e463b5cd7a37800bd20c7
elzieconnor <31282129+elzieconnor@users.noreply.github.com>
2019-04-03 11:37:25 -0500

    Update camera_client.py
    Added import statements
    Added database insertion statements

--- camera_client.py
+++ camera_client.py
@@ -1,31 +1,34 @@
+from app import db
+from app.models import Frame
 import numpy as np
 import cv2
 import math
+import pickle
+import matplotlib.pyplot as plt
+import importlib
+import os, glob
 
 cap = cv2.VideoCapture(0)
-
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-
-frameRate = cap.get(5) #frame rate
-x=1
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (640,480))
 
 while(cap.isOpened()):
-    frameId = cap.get(1) 
     ret, frame = cap.read()
     if ret==True:
-        # write the flipped frame
         out.write(frame)
-
-        filename = './test' +  str(int(x)) + ".jpg";x+=1
-        cv2.imwrite(filename, frame)
 
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            filename = 'test.jpg';
+            cv2.imwrite(filename, frame)
             break
     else:
         break
+u = Frame(frame_fname = filename, video_fname = 'output.mp4')
+db.session.add(u)
+db.session.commit()
+
 # Release everything if job is finished
 cap.release()
 out.release()
