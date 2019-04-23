1dd025d7d4a0418b83cec6773b60cabfef5007c5
devencolbert <31320471+devencolbert@users.noreply.github.com>
2019-04-23 13:17:40 -0500

    Update server

--- cam_server.py
+++ cam_server.py
@@ -3,55 +3,36 @@ from lib.cam import Camera
 import json
 import numpy
 import cv2
-
+FEEDS = {} #Format: {"<ID>: Camera Object"}
+SETUP = False
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def gen():
-    while True:
-        frame = get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame)
-
-@app.route('/get_frame', methods = ['POST', 'GET'])
+@app.route('/get_frame', methods = ['GET'])
 def get_frame():
 	print(request.headers)
+	
 	#STEP-1: Locate specific camera
-	c = Camera('live',0,cam_dict['cam_1'])
-	if request.method == 'GET':
-		addr = request.form
-	print(addr)
+	
 	#STEP-2: Pull-norm-package frame
-	frame = c.package(c.norm_frame(c.get_frame()))
+	#frame = c.package(c.norm_frame(c.get_frame()))
+	
 	#STEP-3: return frame
 	return frame
-"""return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-"""
+	
 
-def server_init():
-	#pass
-	#STEP-01: Setup camera dictionary (global obj)
-	global cam_dict
-	cam_dict = {'cam_1': 0, 'cam_2': 1, 'cam_3': 2, 'cam_4': 3}
-	#STEP-02: Check if cams connected
-	c = Camera('live',0,cam_dict['cam_1'])
-	print(c.cam_open())
-	c = Camera('live',1,cam_dict['cam_2'])
-	print(c.cam_open())
-	c = Camera('live',2,cam_dict['cam_3'])
-	print(c.cam_open())
-	c = Camera('live',3,cam_dict['cam_4'])
-	print(c.cam_open())
+def init():
+	global FEEDS
+	#STEP-01: Build the Feeds Dict from user input
 	
+	#STEP-02: Check video feeds
+	
+	#STEP-03: return True/False if setup failed
+	input('Press Enter to continue...')
+
 if __name__ == '__main__':
-	server_init()
-	app.run(host='0.0.0.0', port='8000', debug=True)
-	
-#STEP 1: Load in, convert, and decompress frame for use
-frame = json.loads(data.decode("utf8"))
-frame = numpy.asarray(frame, numpy.uint8)
-frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+	init()
+	app.run(host='127.0.0.1', port='8080', debug=False)
