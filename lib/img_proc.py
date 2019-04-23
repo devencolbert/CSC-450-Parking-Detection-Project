7b9663b25e9c4f4dad8b13aa2fbd775f0fae2f49
Savon Williams <36686716+SavoSPB@users.noreply.github.com>
2019-04-23 13:40:55 -0500

    made img_proc.py for image processing

---
+++ lib/img_proc.py
@@ -0,0 +1,27 @@
+
+
+class ImgProcessor():
+
+    def init():
+        #setup variable
+        #network
+        #setup parking spots
+
+    def process_frame(frame):
+        #cars = self.detect_cars(....)
+        #available_spots = self.detect_available(...)
+
+        #return available parking spots(results)
+
+    def detect_cars(frame):
+        #detect cars
+        #return bounding boxes
+
+    def detect_available(cars, spots):
+
+
+
+        #return
+
+    def format_spots(avail):
+        #return all_spots
