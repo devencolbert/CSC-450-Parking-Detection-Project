8c8f99dc7f13217dab089cb1fe9fef6097638b61
elzieconnor <31282129+elzieconnor@users.noreply.github.com>
2019-04-24 16:03:03 -0500

    Update application.py

--- application.py
+++ application.py
@@ -7,10 +7,11 @@ from lib.cam import Camera
 import numpy as np
 import requests
 import json
+import yaml
 import cv2
 import os
 
-FEEDS = {}
+inc = 0
 application = Flask(__name__)
 
 application.config.from_object(__name__)
@@ -30,6 +31,18 @@ admin = Admin(application, name = 'Admin Portal', template_mode = 'bootstrap3')
 class ConfigurationView(BaseView):
     @expose('/')
     def index(self):
+        #if request.method == 'POST':
+        data = request.json
+        print(data)
+    #with open('config.yml','r') as yamlfile:
+        #cur_yaml = yaml.load(yamlfile)
+        #cur_yaml.extend(data)
+        #print(cur_yaml)
+
+    #with open('config.yml','w') as yamlfile:
+        #yaml.dump(data, yamlfile, default_flow_style=False)
+                       
+        #return render_template("test.html")
         return self.render('configuration.html')
 
 admin.add_view(ConfigurationView(name='Configuration', endpoint='configuration'))
@@ -39,67 +52,68 @@ admin.add_view(ModelView(Spot, db.session, category="Database"))
 @application.route('/')
 @application.route('/index')
 def index():
-    lot = []
-    #Create connection session between SQLAlchemy database and server
-    #Select ALL records in tables Lot
-    #Store queries in data and push to INDEX template
+    #   Create connection session between SQLAlchemy database and server
+    #   Select ALL records in tables Lot
+    #   Store queries in data and push to INDEX template
     data = db.session.execute("SELECT * FROM Lot").fetchall()
-    #Lot.query.all()
-    #for l in Lot.query.all():
-        #lot.append(l.location)
     return render_template('index.html', data = data)
-    #return json.dumps(lot)
 
 @application.route('/info/<location>')
 def info(lot_id):
     lot_location = location
-    #Create connection session between SQLAlchemy database and server
-    #Select records in table Spot based on LOT_ID parameter
-    #Store queries in data and push to INFO template
-    #data = db.session.execute("SELECT * FROM Spot WHERE location = :lot_location;", {"lot_location": lot_location}).fetchall()
-    Spot.query.filter()
+    #   Create connection session between SQLAlchemy database and server
+    #   Select records in table Spot based on LOT_ID parameter
+    #   Store queries in data and push to INFO template
+    data = db.session.execute("SELECT * FROM Spot WHERE location = :lot_location;", {"lot_location": lot_location}).fetchall()
     return render_template('info.html', data = data)
 
-@application.route('/test_input', methods = ['GET', 'POST'])
-def test():
-    global FEEDS
-    x = True
-    increment = 0
-    while x:
-        i = 'ID'
-        mode = input('Is the video feed from camera "live" or from a "file": ')
-        if mode == 'live':
-            addr = input('Enter the address of the live video as a integer: ')
-            addr = int(addr)
-        else:
-            addr = input('Enter the file name of the video: ')
-        cam_id = input('Enter in camera ID for video feed: ')
-        print(mode)
-        print(addr)
-        FEEDS[i+str(increment)] = Camera(mode, addr, cam_id)
-        print(FEEDS)
-        ans = input('Enter "q" to quit: ')
-        if ans == 'q':
-            x = False
-        increment += 1
-    return "This page will show video feeds eventually"
+@application.route('/show_frame', methods=['GET'])
+def show_frame():
+    #   test_input = test
+    #   var = input('Enter ID: ')
+    #   print(FEEDS[var])
+    #   frame = FEEDS[var].package(FEEDS[var].norm_frame(FEEDS[var].get_frame()))
+    #   print(frame)
+    r = requests.get('http://127.0.0.1:8080/get_frame').content
+    frame = json.loads(r)
+    frame = np.asarray(frame, np.uint8)
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    r, jpg = cv2.imencode('.jpg', frame)
+    return Response(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + jpg.tobytes() + b'\r\n\r\n', mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@application.route('/test_config', methods=['GET'])
-def test_config():
-    #var = input('Enter ID: ')
-    #print(FEEDS[var])
-    #frame = FEEDS[var].package(FEEDS[var].norm_frame(FEEDS[var].get_frame()))
-    #print(frame)
-	r = requests.get('http://127.0.0.1:8080/get_frame').content
-	print(r)
-	frame = json.loads(r)
-	frame = np.asarray(frame, np.uint8)
-	frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-	r, jpg = cv2.imencode('.jpg', frame)
-	#response = make_response(jpg.tobytes())
-    #return Response(FEEDS[var])
-	return Response(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + jpg.tobytes() + b'\r\n\r\n', mimetype='multipart/x-mixed-replace; boundary=frame')
-	#return response
+@application.route('/test', methods=['GET', 'POST'])
+def test_2():
+    global inc
+    stuff = {'id': 0, 'points': []}
+    #if request.method == 'POST':
+    arr = []
+    data = request.json
+    #info['points'] = request.json
+    if data != None:
+        #new_data = len(yaml_loader())
+        print(data[0])
+        stuff['points'] = [list(data[0]), list(data[1]), list(data[2]), list(data[3])]
+        stuff['id'] = inc
+        inc = inc + 1
+        arr.append(stuff)
+        #obj = json.loads(data)
+    #print(data)
+        with open('config.yml','a') as yamlfile:
+            yaml.dump(arr, yamlfile)
+    #cur_yaml = yaml.load(yamlfile)
+    #cur_yaml.extend(data)
+    #print(cur_yaml)
+
+    #with open('config.yml','w') as yamlfile:
+    #yaml.dump(data, yamlfile, default_flow_style=False)
+                       
+    #return render_template("test.html")
+    return render_template("test.html")
+
+def yaml_loader():
+    with open('config.yml', "r") as yamlfile:
+        data2 = yaml.load(yamlfile)
+        return data2
 
 # Accept input for Camera Object ID and Address
 # Timed JSON call (Loop Image Processing Software)
