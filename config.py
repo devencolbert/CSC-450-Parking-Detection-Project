1eaee741b1f91ed3c57d75667f3e025d8b24b113
elzieconnor <31282129+elzieconnor@users.noreply.github.com>
2019-04-30 19:22:35 -0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(PROJECT_ROOT, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = False
-    SECRET_KEY = 'development-key'
